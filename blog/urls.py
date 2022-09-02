2f5b96a8b3db36dcd15f99c89b4701bb14462ca5
DOdrums <dirkornee@hotmail.com>
2022-09-02 15:07:37 +0000

    add functionality for logging in, commenting and liking

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.Postlist.as_view(), name='home'),
-    path('post/<slug:slug>', views.PostDetail.as_view(), name='post_detail')
+    path('post/<slug:slug>', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
