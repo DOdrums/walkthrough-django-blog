93d663ef4d5a722a2558a8cef9940c1fbfd1499d
DOdrums <dirkornee@hotmail.com>
2022-09-02 13:21:51 +0000

    write detail view and template to display content and comments of post

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.Postlist.as_view(), name='home')
+    path('', views.Postlist.as_view(), name='home'),
+    path('post/<slug:slug>', views.PostDetail.as_view(), name='post_detail')
 ]
